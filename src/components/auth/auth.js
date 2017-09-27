1ffe4adfee227a6c2c28a8405b993f5772286497
Ryan Huse <ryanhuse@ksu-wireless-189-1.campus.ksu.edu>
2017-09-27 15:29:35 -0500

    auth and login

--- src/components/auth/auth.js
+++ src/components/auth/auth.js
@@ -1,16 +1,26 @@
 import React, {Component} from 'react';
 import './auth.css';
+import * as firebase from 'firebase';
 
 export default class Auth extends Component {
+
     constructor(props){
         super(props);
         this.state = {user: null}
     }
+    
+    componentWillMount(){ 
+        firebase.auth().onAuthStateChanged((user) => {
+            this.setState({user:user})
+        })
+   }
+    
     render() {
-        if(this.state.  user) {
+        if(this.state.user) {
             return (<div>{this.props.children}</div>);
         } else {
             return (<h1>Login Form!</h1>);
         }
     }
-} 
+}
+                    
