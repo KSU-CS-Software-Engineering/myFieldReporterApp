95a855a49ee0fd76b945ff0e4e232a7af20e4f08
cjoewd <cjoewd@gmail.com>
2017-09-27 15:48:58 -0500

    Sign in Works

--- src/components/login-form/login-form.js
+++ src/components/login-form/login-form.js
@@ -2,6 +2,7 @@
 
 import React, {Component} from 'react';
 import './login-form.css';
+import * as firebase from 'firebase';
 
 export default class LoginForm extends Component {
     constructor(props) {
@@ -11,26 +12,34 @@ export default class LoginForm extends Component {
             password: ''
         }
         this.handleChange = this.handleChange.bind(this);
+        this.handleSignIn = this.handleSignIn.bind(this);
     }
     handleChange(event) {
         this.setState({
             [event.target.name]: event.target.value
         });
+    }
+    handleSignIn() {
+       console.log("HERE");
+        firebase.auth().signInWithEmailAndPassword(this.state.username, this.state.password).catch((err) => {
+            this.setState({message: err.message});
+        })
         console.log(this.state)
     }
     render() {
         return (
             <div className="container">
                 <h1>Login</h1>
-                <form>
-                    <label><b>Username</b></label>
-                    <input type="text" placeholder="Enter Username" name="username" value={this.state.username} onChange={this.handleChange} required />
+            {this.state.message}
+               
+                    <label><b>Email</b></label>
+                    <input type="email" placeholder="Enter Username" name="username" value={this.state.username} onChange={this.handleChange} required />
 
                     <label><b>Password</b></label>
                     <input type="password" placeholder="Enter Password" name="password" value={this.state.password} onChange={this.handleChange} required />
 
-                    <button type="submit">Login</button>
-                </form>
+                    <button onClick={this.handleSignIn}>Login</button>
+              
             </div>
         );
     }
