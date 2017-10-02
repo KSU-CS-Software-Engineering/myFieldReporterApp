3199e0720a8564c4720b50aa3a986762cc38fde4
Ryan Huse <ryanhuse@Ryans-MacBook-Pro.local>
2017-10-02 14:50:19 -0500

    about to start react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.png';
 import './App.css';
+import {Link} from 'react-router-dom';
 import Auth from './components/auth/auth';
 import * as firebase from 'firebase';
 import Fields from './components/fields/fields';
@@ -13,22 +14,26 @@ class App extends Component {
             //Error  
         });
     }
+    handleFields(){
+        <Fields></Fields>
+    }
   render() {
     return (
       <div className="App">
-        <Auth>
-            <div className="App-header">
+        <div className="App-header">
               <img src={logo} className="App-logo" alt="logo" />
-            </div>
+        </div>
+            
+        <Auth>
             <p className="App-intro">
                 
                 <button >New Report</button>
                 </p><p>
-                <button >View Fields</button>
-                </p>
+                <Link to="/fields">View Fields</Link>
+            </p>
                 
          
-                <a href='' onClick={this.handleLogOut}>Log Out</a>
+            <a href='' onClick={this.handleLogOut}>Log Out</a>
            
         </Auth>
       </div>
