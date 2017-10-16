2e134efe7bd668d9ee92c1c8018242941da79dc8
Ryan Huse <ryanhuse@Ryans-MacBook-Pro.local>
2017-10-16 15:52:22 -0500

    added camera on computer

--- src/components/reports/reports.js
+++ src/components/reports/reports.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import './reports.css';
 import * as firebase from 'firebase';
 import {HashRouter as Router, Route, Switch, Link} from 'react-router-dom';
-
+import Webcam from 'react-webcam';
 
 
 export default class reports extends Component {
@@ -12,7 +12,7 @@ export default class reports extends Component {
             crop: '',
             gs: '',
             location: '',
-            picture: '',
+            images: [],
             pest: '',
             notes: '',
             view: 'current',
@@ -21,9 +21,14 @@ export default class reports extends Component {
         this.handleCreate = this.handleCreate.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.toggleView = this.toggleView.bind(this);
-        
+        this.capture = this.capture.bind(this);
+        this.setRef = this.setRef.bind(this);
         this.getReports();
         
+    }
+    
+    setRef(webcam){ 
+        this.webcam = webcam;
     }
     
     handleChange(event) {
@@ -34,19 +39,41 @@ export default class reports extends Component {
     handleCreate(){
         var fid = firebase.database().ref('reports/').push().key;
         var uid = firebase.auth().currentUser.uid;
+        
         var updates = {}
         updates['reports/' + fid] = {
-            field: this.state.field,
             crop: this.state.crop,
             location: this.state.location,
             owner: uid
           }
-        updates['users/' + uid + /reports/ + fid] = true;
-        firebase.database().ref().update(updates).catch(err => console.error(err));
-            this.toggleView();
+        updates['users/' + uid + '/reports/' + fid] = true;
+        firebase.database().ref().update(updates).then(() => {
+            this.state.images.forEach( (imageURL, index) => {
+            var blob = dataURItoBlob(imageURL);
+                firebase.storage().ref().child('images').child(fid).child(index.toString()).put(blob).then(snapshot => {
+                    console.log(snapshot)
+                    firebase.database().ref('reports/' + fid + '/images').push(snapshot.downloadURL)
+                })
+            });
+            
+        }).catch(err => console.error(err));
+        
+        this.toggleView();
+        
+        this.setState({
+            crop: '',
+            gs: '',
+            location: '',
+            images: [],
+            pest: '',
+            notes: '',
+            view: 'current',
+            list: []
+        })
         
         
     }
+    
     
     componentWillMount(){
         firebase.database().ref('crops').once('value', (snapshot) => {
@@ -65,6 +92,14 @@ export default class reports extends Component {
         this.setState({view:(this.state.view== 'newReport')?'current':'newReport'})
     }
     
+    capture(){
+        const imageSrc = this.webcam.getScreenshot();
+        var images = this.state.images;
+        if(images.length === 2) images.shift();
+        images.push(imageSrc);
+        this.setState({images:images})
+        
+    }
 
     render() {
            
@@ -92,6 +127,9 @@ export default class reports extends Component {
                            console.log(this.state.list);
                            console.log(this.state.list.pop());
                         })
+                    var imageTags = this.state.images.map((imageURL, index) => {
+                        return <img key={index} src={imageURL}/>
+                    })
                     return(
                          
                         <div className="container">
@@ -107,7 +145,11 @@ export default class reports extends Component {
                             <br/>
                             <input placeholder="Pest" name="pest" value={this.state.pest} onChange={this.handleChange} required />
                             <br/>
-                            <input placeholder="Picture" name="picture" value={this.state.picture} onChange={this.handleChange} required/>
+                            
+                            {imageTags}
+                            
+                             <Webcam audio={false} height={350} ref={this.setRef} screenshotFormat="image/jpeg" width={350} />
+                            <button placeholder="Picture" name="picture" value={this.state.picture} onClick={this.capture} required/>
                             <br/>
                             <input placeholder="Notes" name="notes" value={this.state.notes} onChange={this.handleChange}/>
                             
@@ -124,4 +166,29 @@ export default class reports extends Component {
                 }
     }
     
+}
+
+function dataURItoBlob(dataURI) {
+  // convert base64 to raw binary data held in a string
+  // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+  var byteString = atob(dataURI.split(',')[1]);
+
+  // separate out the mime component
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+  // write the bytes of the string to an ArrayBuffer
+  var ab = new ArrayBuffer(byteString.length);
+
+  // create a view into the buffer
+  var ia = new Uint8Array(ab);
+
+  // set the bytes of the buffer to the correct values
+  for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+  }
+
+  // write the ArrayBuffer to a blob, and you're done
+  var blob = new Blob([ab], {type: mimeString});
+  return blob;
+
 }
