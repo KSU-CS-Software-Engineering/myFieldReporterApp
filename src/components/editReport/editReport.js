1c63f6902517261192dfdd103fc7ea7706910c75
Ryan Huse <ryanhuse@ksu-wireless-185-114.campus.ksu.edu>
2017-11-15 13:57:14 -0600

    add edit report

---
+++ src/components/editReport/editReport.js
@@ -0,0 +1,205 @@
+import React, {Component} from 'react';
+import './editReport.css';
+import Dashboard from '../dashboard/dashboard';
+import SearchableList from '../searchableList/searchableList';
+import * as firebase from 'firebase';
+import {HashRouter as Router, Route, Switch, Link} from 'react-router-dom';
+
+export default class editReports extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            crop: '',
+            gs: '',
+            location: '',
+            images: [],
+            pest: '',
+            notes: '',
+            view: 'current',
+            list: [],
+            reports: [],
+            test: '',
+            reportName: '',
+            reportID: ''
+        }
+        this.handleCreate = this.handleCreate.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSelect = this.handleSelect.bind(this);
+        this.readFile = this.readFile.bind(this);
+        
+    }
+    
+    componentWillMount() {
+        
+        if(this.props.reportID){
+            firebase.database().ref('reports/' + this.props.reportID).once('value').then((snapshot) =>{
+               var report = snapshot.val();
+                console.log
+                this.setState({
+                    reportID: this.props.reportID,
+                    reportName: report.name,
+                    crop: report.crop,
+                    gs: report.gs,
+                    pest: report.pest,
+                    notes: report.notes,
+                    time: report.time,
+                    images: report.images,
+                    location: report.location
+                              
+                });
+                
+            });
+        }  
+    }
+    
+
+    //Change state values with whatever was entered. if crop is the name, crop value will be changed.
+    handleChange(event) { 
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+    
+
+    //Change a specific state with a specific value. Used in searchableList
+    handleSelect(name, value){ 
+        this.setState({[name]: value})
+    }
+    
+    //Creates the entry for the database from the state objects when submit is clicked
+    handleCreate(){
+        var photos = this.state.images;
+        var uid = firebase.auth().currentUser.uid;
+        var state = this.state;
+              var updates = {}
+              updates['reports/' + this.state.reportID] = {
+                    crop: state.crop,
+                    gs: state.gs,
+                    pest: state.pest,
+                    notes: state.notes,
+                    time: new Date().toJSON(),
+                    owner: uid,
+                    name: this.state.reportName,
+                    location: state.location,
+                    images: state.images
+                  }
+                updates['users/' + uid + '/reports/' + this.props.reportID] = true;
+            console.log('updates', updates)
+                firebase.database().ref().update(updates);
+        
+        
+        
+        this.setState({
+            crop: '',
+            gs: '',
+            location: '',
+            images: [],
+            pest: '',
+            notes: '',
+            view: 'current',
+            list: []
+        })
+        
+        
+    }
+    
+    
+    //Processes the image selected from user below
+    readFile(event) {
+        var file = event.target.files[0];
+        var reader = new FileReader();
+
+        reader.onloadend = () => {
+            this.state.images[this.state.images.length] = file;
+            
+        }
+
+        reader.onerror = function () {
+            alert('There was an error reading the file!');
+        }
+        reader.readAsDataURL(file);
+    }
+
+    
+    
+    render() {
+        if (window.File && window.FileReader && window.FormData) {
+            var $inputField = this.state.file;
+
+        } else {
+            alert("File upload is not supported!");
+
+        }
+
+
+
+
+        firebase.database().ref('crops/').on('value', snap =>  {
+               var data = [];
+               snap.forEach(ss => {
+                  data.push(ss.child('name').val());
+               });
+                this.state.list = data;
+            })
+
+        return(
+
+            <div className="reports-container">
+                <h1>{this.state.reportName}</h1>
+                
+                <div className="report-section-wrap">
+                    <div className="report-header-wrap">
+                        <label className="bold">Crop:&nbsp;</label>
+                    </div>
+                    <div className="report-info-wrap">
+                        <div className="list-container">
+                            <SearchableList onChange={(term) => this.handleSelect('crop', term)} placeholder='Crop' listRef="crops/" value={this.state.crop}/>
+                        </div>
+                    </div>
+                </div>
+                <div className="report-section-wrap">
+                    <div className="report-header-wrap">
+                        <label className="bold">Growth Stage:&nbsp;</label>
+                    </div>
+                    <div className="report-info-wrap">
+                        <input placeholder="Growth Stage of Crop" name="gs" value={this.state.gs} onChange={this.handleChange} required/>
+                    </div>
+                </div>
+                <div className="report-section-wrap">
+                    <div className="report-header-wrap">
+                        <label className="bold">Pests:&nbsp;</label>
+                    </div>
+                    <div className="report-info-wrap">
+                        <input placeholder="Pest" name="pest" value={this.state.pest} onChange={this.handleChange} required />
+                    </div>
+                </div>
+                <div className="report-section-wrap">
+                    <div className="report-header-wrap">
+                        <label className="bold">Notes:&nbsp;</label>
+                    </div>
+                    <div className="report-info-wrap">
+                        <input placeholder="Notes" name="notes" value={this.state.notes} onChange={this.handleChange}/>
+                    </div>
+                </div>
+                
+                
+
+
+                
+                <br/>
+
+
+
+
+                
+                <button onClick={this.handleCreate}>Submit</button>
+
+                {this.state.message}
+
+                <Link to="/">Go To Dashboard</Link>
+            </div>
+
+        )
+    }
+    
+}
