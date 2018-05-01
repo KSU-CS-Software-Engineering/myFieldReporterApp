56bd32556bb2db3ff9adb8c0abcb07b94cdf372a
CjoewD <cjoewd@gmail.com>
2018-05-01 09:58:28 -0500

    added loading screen

---
+++ src/components/loading/loading.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import './loading.css';
+import logo from '../../logo.png';
+
+
+//File for the Create Report button. It will bring up all input fields necessary for a report
+export default class Reports extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+
+
+    }
+
+
+        return(
+
+            <div className="loading-container">
+                <div className="align-helper"></div>
+                <img src={logo} alt="logo" />
+            </div>
+
+        )
+
+    }
+
+}
+
+/*
+    <label>Distribution:</label>
+    <input type="radio" name="dist" value="Uniform" onChange={this.handleChange} className="dist"></input>Uniform
+    <input type="radio" name="dist" value="Patchy" onChange={this.handleChange} className="dist"></input>Patchy
+    <br/>
+    <label>Severity:</label>
+    <input type="radio" name="sevr" value="Low" onChange={this.handleChange} className="dist"/>Low
+    <input type="radio" name="sevr" value="Medium" onChange={this.handleChange} className="dist"/>Medium
+    <input type="radio" name="sevr" value="High" onChange={this.handleChange} className="dist"/>High
+*/
