dee6a5fac9d4857d1db4d6537f3d15da71164532
Ryan Huse <rmhks95@ksu.edu>
2018-05-01 10:27:12 -0500

    won't pull if offline

--- src/App.js
+++ src/App.js
@@ -35,12 +35,16 @@ class App extends Component {
     }
 
   componentWillMount(){
-    if(navigator.onLine){
+    if(window.navigator.onLine){
+      console.log("offline");
       var db = firebase.firestore();
       var crops = {};
       db.collection("data").doc("crops").get().then((snapshot)=>{
         this.setState({crops: snapshot.data(), ready: true});
       });
+    }
+    else{
+      this.setState({ready: true});
     }
   }
 
